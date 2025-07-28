86c9fdb6eef5929103494e1a3548103a30bd6124
FinalGrabacr <diogomvacarvalho@gmail.com>
2025-07-28 20:45:38 +0100

    2

--- games/pong/pong.js
+++ games/pong/pong.js
@@ -19,10 +19,40 @@ class PongGame {
         this.gameSpeed = 300; // milliseconds
         this.isGameOver = true;
         this.lastDirection = 0;
+
+        this.paddleWidth = 1;
+        this.paddleHeight = 5;
+
+        this.leftPaddleY = Math.floor((this.boardHeight - this.paddleHeight) / 2);
+        this.rightPaddleY = Math.floor((this.boardHeight - this.paddleHeight) / 2);
+
     }
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        this.ctx.fillStyle = '#000';
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+        this.ctx.fillStyle = '#fff';
+
+        for (let i = 0; i < this.paddleHeight; i++) {
+            this.ctx.fillRect(
+                1 * this.gridSize,
+                (this.leftPaddleY + i) * this.gridSize,
+                this.gridSize,
+                this.gridSize
+            );
+        }
+
+        for (let i = 0; i < this.paddleHeight; i++) {
+            this.ctx.fillRect(
+                (this.boardWidth - 2) * this.gridSize,
+                (this.rightPaddleY + i) * this.gridSize,
+                this.gridSize,
+                this.gridSize
+            )
+        }
 
 
     }
